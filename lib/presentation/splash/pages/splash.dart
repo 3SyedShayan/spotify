3a3f8e5b363884283283f134208b6b0ffa586e53
Syed Shayan <3syedshayan@gmail.com>
2025-03-26 23:51:52 +0500

    Created Spash Screen

---
+++ lib/presentation/splash/pages/splash.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/presentation/intro/pages/get_started.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: SvgPicture.asset(AppVectors.logo)));
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (BuildContext context) => GetStartedPage()),
+    );
+  }
+}
